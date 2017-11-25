5fc0df40cb4c7bfd5b38991b33d148a7eb6f1452
davidkneely <davidics613@gmail.com>
2017-11-24 18:22:30 -1000

    Initial Commit

---
+++ popupDialog002/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  popupDialog002
+//
+//  Created by David on 11/24/17.
+//  Copyright © 2017 Vision Runner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
